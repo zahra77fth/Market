485046da14baf588da0a22cb0dcec14f0168fa80
Afagh <afagh@Afaghs-MacBook-Pro.local>
2025-01-27 14:25:56 +0330

    feat: initial deploy

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { APP_NAME } from '../lib/constants';
-import Image from 'next/image';
-import { Button } from '../components/ui/button';
+// import { APP_NAME } from '../lib/constants';
+// import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 const NotFoundPage = () => {
     return (
